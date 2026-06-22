3526c2af2f87171f634fbc1a5fa727f609064418
William Steinberg <brotherbill@mail.com>
2026-06-22 11:24:00 -0400

    c18 populated

---
+++ c11-20/c18/c18_p82_2_redirecting_the_standard_input_from_a_file_with_operator_lt/app.d
@@ -0,0 +1,9 @@
+import std.stdio : readf, writeln;
+
+void main()
+{
+	double number;
+
+	readf(" %s", &number);
+	writeln(number / 10);
+}
---
+++ c11-20/c18/c18_p83_4a_piping_programs_with_operator_pipe_by_two/app.d
@@ -0,0 +1,9 @@
+import std.stdio;
+
+void main()
+{
+	double number;
+
+	readf(" %s", &number);
+	writeln(number * 2);
+}
